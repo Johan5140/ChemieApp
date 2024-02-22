d8304dfb5d55bdfe746bee01b9a571ca1d861dca
Johan5140 <Johan5140@local>
2024-02-22 12:46:35 +0100

    Optimalizace

--- ChemieApp/Form2.Designer.cs
+++ ChemieApp/Form2.Designer.cs
@@ -39,11 +39,12 @@
             // 
             // button1
             // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(362, 416);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "ZAVŘÍT";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
